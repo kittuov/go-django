63d3e57be0dc470fb8d698eea6a42351eaa1cd31
kittuov <kittuov@gmail.com>
2016-05-24 05:11:01 +0530

    added tests

---
+++ admin/doc.go
@@ -0,0 +1,11 @@
+/*
+The admin package resembles Django's admin package.
+
+* it has some salient features that makes it unique.
+1. TODO: It is real-time.
+2. TODO: it has push notification support.
+3. Offline page. so works even if your server is down.
+4. loads data with ajax. lower data, lower battery usage,
+5. uses polymer for data binding. so easy to manage and edit pages. 
+*/
+package admin
