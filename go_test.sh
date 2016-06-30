222ba01878ce5159bf06f8016edea37adbcce05f
kittuov <kittuov@gmail.com>
2016-06-30 23:26:30 +0530

    one more try!

--- go_test.sh
+++ go_test.sh
@@ -3,6 +3,6 @@
 PACKAGES=$(find ./ -type d -not -path '*/\.*' -not -path "*/\webapp*")
 echo $PACKAGES
 for pkg in $PACKAGES;do
-go test -coverprofile=coverage-one.out -covermode=count $pkg
-tail -n +2 coverage-one.out >> coverage.out
+go test -coverprofile=coverage-one.out -covermode=atomic $pkg
+tail -n +2 coverage-one.out >> coverage.txt
 done
