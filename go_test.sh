d5f3cd53bda0ddf454b723180768c2d5d4ae34a0
kittuov <kittuov@gmail.com>
2016-07-01 00:00:57 +0530

    trying with cat

--- go_test.sh
+++ go_test.sh
@@ -4,6 +4,6 @@ PACKAGES=$(find ./ -type d -not -path '*/\.*' -not -path "*/\webapp*")
 echo $PACKAGES
 for pkg in $PACKAGES;do
 if go test -coverprofile=coverage-one.out -covermode=atomic $pkg; then
-tail -n +2 coverage-one.out >> coverage.txt
+cat coverage-one.out >> coverage.txt
 fi
 done
